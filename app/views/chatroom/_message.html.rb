c58f1e6e404fc1ec543cafafcf97eb5000bfabc1
uriel <uriel781@gmail.com>
2018-02-03 00:02:12 -0600

    Chatroom code
    Chatroom index is the main chatroom.
    
    Chatroom controller loads message at login.
    
    _message is a partial to render individual messages which is also used
    in the controller.

---
+++ app/views/chatroom/_message.html.rb
@@ -0,0 +1,8 @@
+
+<div class="event">
+           <div class="content">
+                      <div class="summary">
+                                 <a>Stevie Feliciano</a> added <a>Elliot Fu</a> as a friend
+                             </div>
+            </div>
+                             </div>
